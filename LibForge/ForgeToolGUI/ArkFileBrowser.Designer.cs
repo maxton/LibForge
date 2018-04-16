1c553fe27841f3522de1df7162fc9f7094af0f54
maxton <maxton@maxton.xyz>
2018-04-16 15:33:30 -0400

    Add GUI browser with texture preview

---
+++ LibForge/ForgeToolGUI/ArkFileBrowser.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace ForgeToolGUI
+{
+  partial class ArkFileBrowser
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.treeView1 = new System.Windows.Forms.TreeView();
+      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+      this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.menuStrip1.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // treeView1
+      // 
+      this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+      this.treeView1.Location = new System.Drawing.Point(12, 27);
+      this.treeView1.Name = "treeView1";
+      this.treeView1.Size = new System.Drawing.Size(378, 464);
+      this.treeView1.TabIndex = 0;
+      this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+      // 
+      // menuStrip1
+      // 
+      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+      this.menuStrip1.Name = "menuStrip1";
+      this.menuStrip1.Size = new System.Drawing.Size(812, 24);
+      this.menuStrip1.TabIndex = 1;
+      this.menuStrip1.Text = "menuStrip1";
+      // 
+      // fileToolStripMenuItem
+      // 
+      this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.closeToolStripMenuItem,
+            this.exitToolStripMenuItem});
+      this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+      this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+      this.fileToolStripMenuItem.Text = "&File";
+      // 
+      // openToolStripMenuItem
+      // 
+      this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+      this.openToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+      this.openToolStripMenuItem.Text = "&Open...";
+      this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+      // 
+      // closeToolStripMenuItem
+      // 
+      this.closeToolStripMenuItem.Enabled = false;
+      this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+      this.closeToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+      this.closeToolStripMenuItem.Text = "&Close";
+      this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+      // 
+      // exitToolStripMenuItem
+      // 
+      this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+      this.exitToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+      this.exitToolStripMenuItem.Text = "E&xit";
+      this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.pictureBox1.Location = new System.Drawing.Point(396, 27);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(404, 464);
+      this.pictureBox1.TabIndex = 2;
+      this.pictureBox1.TabStop = false;
+      // 
+      // ArkFileBrowser
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(812, 503);
+      this.Controls.Add(this.pictureBox1);
+      this.Controls.Add(this.treeView1);
+      this.Controls.Add(this.menuStrip1);
+      this.MainMenuStrip = this.menuStrip1;
+      this.Name = "ArkFileBrowser";
+      this.ShowIcon = false;
+      this.Text = "Ark File Browser";
+      this.menuStrip1.ResumeLayout(false);
+      this.menuStrip1.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TreeView treeView1;
+    private System.Windows.Forms.MenuStrip menuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    private System.Windows.Forms.PictureBox pictureBox1;
+  }
+}
+
---
+++ LibForge/ForgeToolGUI/ArkFileBrowser.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using LibForge.Texture;
+
+namespace ForgeToolGUI
+{
+  public partial class ArkFileBrowser : Form
+  {
+    public ArkFileBrowserState state;
+    public ArkFileBrowser()
+    {
+      state = new ArkFileBrowserState();
+      InitializeComponent();
+    }
+
+    private void LoadArk(string filename)
+    {
+      if (state.ark != null) CloseArk();
+      state.ark = GameArchives.Ark.ArkPackage.OpenFile(GameArchives.Util.LocalFile(filename));
+      closeToolStripMenuItem.Enabled = true;
+      void AddNodes(GameArchives.IDirectory dir, TreeNodeCollection nodes)
+      {
+        foreach (var d in dir.Dirs)
+        {
+          var node = new TreeNode(d.Name);
+          nodes.Add(node);
+          AddNodes(d, node.Nodes);
+        }
+        foreach(var f in dir.Files)
+        {
+          var node = new TreeNode(f.Name);
+          node.Tag = f;
+          nodes.Add(node);
+        }
+      }
+      AddNodes(state.ark.RootDirectory, treeView1.Nodes);
+    }
+
+    private void CloseArk()
+    {
+      if (state.ark == null) return;
+      treeView1.Nodes.Clear();
+      state.ark.Dispose();
+      state.ark = null;
+      closeToolStripMenuItem.Enabled = false;
+    }
+
+    private void openToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      OpenFileDialog of = new OpenFileDialog();
+      of.Filter = "Ark Header (*.hdr)|*.hdr";
+      if(of.ShowDialog(this) == DialogResult.OK)
+      {
+        LoadArk(of.FileName);
+      }
+    }
+
+    private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      CloseArk();
+    }
+
+    private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      Application.Exit();
+    }
+
+    private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+    {
+      switch (e.Node.Tag)
+      {
+        case null:
+          break;
+        case GameArchives.IFile i:
+          if (i.Name.Contains(".bmp_") || i.Name.Contains(".png_"))
+          {
+            using (var s = i.GetStream())
+            {
+              var tex = TextureReader.ReadStream(s);
+              pictureBox1.Image = TextureConverter.ToBitmap(tex, 0);
+            }
+          }
+          break;
+      }
+    }
+  }
+  public class ArkFileBrowserState
+  {
+    public GameArchives.Ark.ArkPackage ark;
+  }
+}
