9bf1a7671ac64e47aa4a6710c020bffd1d18bd40
maxton <maxton@maxton.xyz>
2018-05-28 15:36:47 -0400

    [LibForge] Add placeholder rbsong writer

---
+++ LibForge/LibForge/RBSong/RBSongWriter.cs
@@ -0,0 +1,19 @@
+﻿using LibForge.Util;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LibForge.RBSong
+{
+  class RBSongWriter : WriterBase<RBSong>
+  {
+    public RBSongWriter(Stream s) : base(s) { }
+    public override void WriteStream(RBSong v)
+    {
+      Write(0xE);
+      Write(0);
+    }
+  }
+}
