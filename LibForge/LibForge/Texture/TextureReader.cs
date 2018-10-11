46e43ffb74e6970b3c101f7a6443b41e482f1009
Maxton <maxton@maxton.xyz>
2018-10-11 19:26:48 -0400

    [LibForge] Support textures from the latest update

--- LibForge/LibForge/Texture/TextureReader.cs
+++ LibForge/LibForge/Texture/TextureReader.cs
@@ -17,20 +17,21 @@ namespace LibForge.Texture
 
     public override Texture Read()
     {
-      var version = Int();
-      if(version != 6 && version != 4)
+      var magic = Int();
+      if(magic != 6 && magic != 4)
       {
-        throw new Exception($"Unknown texture version {version}");
+        throw new Exception($"Unknown texture magic {magic}");
       }
-      var hdrData = version == 6 ? FixedArr(Byte, 0xB0) : FixedArr(Byte, 0xA8);
+      var version = Int();
+      var hdrData = magic == 6 ? FixedArr(Byte, version == 0xC ? 0x7Cu : 0xACu) : FixedArr(Byte, 0xA8);
       var MipmapLevels = UInt();
       var Mipmaps = FixedArr(() => new Texture.Mipmap
       {
         Width = Int(),
         Height = Int(),
-        Flags = Int()
+        Flags = version == 0xC ? Int().Then(Skip(8)) : Int()
       }, MipmapLevels);
-      Check(UInt(), 6u);
+      UInt();
       for(var i = 0; i < Mipmaps.Length; i++)
       {
         Mipmaps[i].Data = Arr(Byte);
@@ -38,7 +39,7 @@ namespace LibForge.Texture
       var footerData = FixedArr(Byte, 0x1C);
       return new Texture
       {
-        Version = version,
+        Version = magic,
         Mipmaps = Mipmaps,
         HeaderData = hdrData,
         FooterData = footerData
